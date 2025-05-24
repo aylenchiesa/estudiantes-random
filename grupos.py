04ed01bc9037b8545a9400c936665fddf7b2d463
Aylen Meza Chiesa <130184706+aylenchiesa@users.noreply.github.com>
2025-05-23 22:39:33 -0300

    Update grupos.py

--- grupos.py
+++ grupos.py
@@ -16,6 +16,10 @@ random.shuffle(estudiantes)
 grupo1 = estudiantes[:18]
 grupo2 = estudiantes[18:35]
 
-print("Grupo 1: ", grupo1)
+print("Grupo 1: ")
+for i in grupo1:
+    print("-", i)
 print()
-print("Grupo 2: ", grupo2)
+print("Grupo 2: ")
+for i in grupo2:
+    print("-", i)
