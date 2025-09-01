0a6f37755afda994fc035474f47f7130f0f359b3
Aylen Meza Chiesa <130184706+aylenchiesa@users.noreply.github.com>
2025-09-01 15:27:06 -0300

    Update grupos.py

--- grupos.py
+++ grupos.py
@@ -1,14 +1,7 @@
 import random
 
 estudiantes = [
-    "Abelenda Ana Paula", "Alcaràz Pikarski Nicolás Agustìn", "Arostegui Simón", "Asuad Manuel", "Azcona Matilda Lorena",
-    "Benitez Juan Pablo", "Bladilo Victoria", "Britez Ignacio Tobias", "Cáceres Pared Marcelo Agustín", "Caminos Roberto Damian",
-    "Codermatz Juan Valentino", "Codutti Laura Agostina", "Correa Aylen Agostina", "Costantini Matias Emanuel",
-    "Diez Noelia Tatiana", "Galvez Juan", "Gauto Rosario", "Gomez Jeronimo Adolfo Javier", "Invaldi Juan Martin",
-    "Kaposvari Franco", "Karatanasopuloz Julian", "Lopez Lecube Lautaro", "Maidana Lautaro Nahuel", "Meza Chiesa Aylen Giovana",
-    "Meza Santiago Exequiel", "Mordka Lucas Ayén", "Mortola Jorge Homero", "Mortola Santino", "Pzocik Erwin Nahum",
-    "Quetglas Agustin", "Repka Soto Máximo", "Romero Stach Lautaro", "Segovia Juan Tomas", "Sosa Wilka Francisco Raul",
-    "Soto Juan Bautista"
+    "x"
 ]
 
 random.shuffle(estudiantes)
@@ -23,3 +16,4 @@ print()
 print("Grupo 2: ")
 for i in grupo2:
     print("-", i)
+
